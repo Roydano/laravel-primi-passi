6e68459e98facb7e4e4adb8aa9b94619c0ec036d
Roydano <roy.dano90@gmail.com>
2021-09-03 18:00:36 +0200

    init + creazione link

---
+++ resources/views/home.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel - I Primi Passi
+                </div>
+
+                <div class="links">
+                    <a href="{{ route('homepage') }}">Home</a>
+                    <a href="{{ route('about-me') }}">About Me</a>
+                    <a href="{{ route('contact') }}">Contact</a>
+                    
+                </div>
+
+                <h2>home</h2>
+            </div>
+        </div>
+    </body>
+</html>
